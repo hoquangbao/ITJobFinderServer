b8b7bbd3e62e8e113e9fed0a0cf8fc37357120eb
hoquangbao1@gmail.com <hoquangbao1@gmail.com>
2020-06-19 16:36:21 +0700

    init project

--- src/module/user/user.route.js
+++ src/module/user/user.route.js
@@ -2,15 +2,15 @@ import { Router } from 'express';
 import validate from 'express-validation';
 import * as userController from './user.controller';
 import userValidation from './user.validate';
-import { authJwt } from '../../service/passport';
+// import { authJwt } from '../../service/passport';
 
 const routes = new Router();
 
-routes.get('/', authJwt, userController.getListUser);
-routes.post('/login', authJwt, userController.login);
-routes.get('/:username', authJwt, userController.getUserDetail);
-routes.patch('/:username', authJwt, validate(userValidation.editProfile), userController.updateUser);
-routes.delete('/:username', authJwt, userController.deleteUser);
+routes.get('/', userController.getListUser);
+routes.post('/login', userController.login);
+routes.get('/:username', userController.getUserDetail);
+routes.patch('/:username', validate(userValidation.editProfile), userController.updateUser);
+routes.delete('/:username', userController.deleteUser);
 routes.post('/register', validate(userValidation.createUser), userController.register);
 
 export default routes;
