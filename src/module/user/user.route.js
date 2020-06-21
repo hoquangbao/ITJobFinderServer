87521761179f1ed3a4c909e586a74efc45366d8e
hoquangbao1@gmail.com <hoquangbao1@gmail.com>
2020-06-21 11:26:04 +0700

    baohq2 change

--- src/module/user/user.route.js
+++ src/module/user/user.route.js
@@ -2,12 +2,12 @@ import { Router } from 'express';
 import validate from 'express-validation';
 import * as userController from './user.controller';
 import userValidation from './user.validate';
-// import { authJwt } from '../../service/passport';
+import { authJwt } from '../../service/passport';
 
 const routes = new Router();
 
 routes.get('/', userController.getListUser);
-routes.post('/login', userController.login);
+routes.post('/login', authJwt);
 routes.get('/:username', userController.getUserDetail);
 routes.patch('/:username', validate(userValidation.editProfile), userController.updateUser);
 routes.delete('/:username', userController.deleteUser);
