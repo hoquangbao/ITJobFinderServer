e2c5caa0358624d26b87cfca34ca774b8d9f19d6
hoquangbao1@gmail.com <hoquangbao1@gmail.com>
2020-06-23 13:36:01 +0700

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,11 +1,11 @@
 const devConfig = {
   MONGO_URL: 'mongodb://localhost:27017/IT-Job-Finder',
-  JWT_SECRET: 'cau-troi-xai-duoc',
+  JWT_SECRET: 'Ayo-MotherFucker',
 };
 
 const prodConfig = {
   MONGO_URL: 'mongodb://baohq:bao0932002321@ds263927.mlab.com:63927/itjobfinder',
-  JWT_SECRET: 'cau-troi-xai-duoc',
+  JWT_SECRET: 'Ayo-MotherFucker',
 };
 
 const defaultConfig = {
