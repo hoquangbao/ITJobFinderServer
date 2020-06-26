1b55c1475e759e283f595f2bb141438f0fc664cf
hoquangbao1@gmail.com <hoquangbao1@gmail.com>
2020-06-26 16:52:23 +0700

    Update company.route.js

--- src/module/company/company.route.js
+++ src/module/company/company.route.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 // import validate from 'express-validation';
 import * as companyController from './company.controller';
+import { authJwt } from '../../service/passport';
+import { roleEmployer } from '../../service/role';
 // import companyValidation from './company.validate';
 
 const routes = new Router();
 
-routes.post('/create_company', companyController.createCompany);
+routes.post('/create_company', authJwt, roleEmployer, companyController.createCompany);
 routes.get('/', companyController.getListCompany);
 routes.get('/:id', companyController.getCompanyDetail);
 routes.patch('/:id', companyController.updateCompany);
