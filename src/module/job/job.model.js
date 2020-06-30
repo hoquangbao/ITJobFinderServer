e85757fc252ee6c5f89a357dc3dc913ed12e039f
hoquangbao1@gmail.com <hoquangbao1@gmail.com>
2020-06-30 16:44:28 +0700

    Update job.model.js

--- src/module/job/job.model.js
+++ src/module/job/job.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
+import jwt from 'jsonwebtoken';
+import constants from '../../config/constants';
 
 const JobSchema = new Schema({
   jobName: {
@@ -28,5 +30,54 @@ const JobSchema = new Schema({
 }, {
   timestamps: true,
 });
+
+JobSchema.pre('save', function (next) {
+  if (this.isModified('password')) {
+    this.password = this.hashPassword(this.password);
+  }
+  return next();
+});
+
+JobSchema.methods = {
+  generateJWT(lifespan) {
+    const today = new Date();
+    const expirationDate = new Date(today);
+    expirationDate.setDate(today.getDate() + lifespan);
+
+    return jwt.sign(
+      {
+        _id: this._id,
+        role: 'job',
+        exp: parseInt(expirationDate.getTime() / 1000, 10),
+      },
+      constants.JWT_SECRET,
+    );
+  },
+  toJSON() {
+    return {
+      _id: this._id,
+      jobDescription: this.jobDescription,
+      salary: this.salary,
+      companyId: this.companyId,
+      userId: this.userId,
+    };
+  },
+  toAuthJSON() {
+    return {
+      ...this.toJSON(),
+      token: this.generateJWT(10000),
+    };
+  },
+};
+
+JobSchema.index({ fullname: 'text' });
+
+JobSchema.statics = {
+  list({ search, queries } = {}) {
+    return search ?
+      this.find(queries, { score: { $meta: 'textScore' } }).sort({ score: { $meta: 'textScore' } }) :
+      this.find(queries).sort({ fullname: 1 });
+  },
+};
 
 export default mongoose.model('Job', JobSchema);
