a18a867fabbc2694a38fea682b4f50d1941c646b
hoquangbao1@gmail.com <hoquangbao1@gmail.com>
2020-06-30 16:44:09 +0700

    Update company.route.js

--- src/module/company/company.route.js
+++ src/module/company/company.route.js
@@ -2,15 +2,15 @@ import { Router } from 'express';
 // import validate from 'express-validation';
 import * as companyController from './company.controller';
 import { authJwt } from '../../service/passport';
-import { roleEmployer } from '../../service/role';
+import { roleEmployer, roleAdmin } from '../../service/role';
 // import companyValidation from './company.validate';
 
 const routes = new Router();
 
 routes.post('/create_company', authJwt, roleEmployer, companyController.createCompany);
-routes.get('/', companyController.getListCompany);
-routes.get('/:id', companyController.getCompanyDetail);
-routes.patch('/:id', companyController.updateCompany);
-routes.delete('/:id', companyController.deleteCompany);
+routes.get('/', authJwt, roleAdmin, companyController.getListCompany);
+routes.get('/:id', authJwt, roleEmployer, companyController.getCompanyDetail);
+routes.patch('/:id', authJwt, roleEmployer, companyController.updateCompany);
+routes.delete('/:id', authJwt, roleEmployer, companyController.deleteCompany);
 
 export default routes;
