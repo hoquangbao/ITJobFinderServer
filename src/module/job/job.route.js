427e728cdeb86dbfd8c519bf962eb4ed7e201be7
hoquangbao1@gmail.com <hoquangbao1@gmail.com>
2020-07-01 09:40:17 +0700

    Update job.route.js

--- src/module/job/job.route.js
+++ src/module/job/job.route.js
@@ -2,11 +2,12 @@ import { Router } from 'express';
 import validate from 'express-validation';
 import * as jobController from './job.controller';
 import jobValidate from './job.validate';
+import { authJwt } from '../../service/passport';
 // import { roleEmployer } from '../../service/role';
 
 const routes = new Router();
 
-routes.post('/create_job', validate(jobValidate.createJob), jobController.createJob);
+routes.post('/create_job', authJwt, validate(jobValidate.createJob), jobController.createJob);
 routes.get('/', jobController.getListJob);
 routes.get('/:id', jobController.getJobDetail);
 routes.patch('/:id', jobController.updateJob);
