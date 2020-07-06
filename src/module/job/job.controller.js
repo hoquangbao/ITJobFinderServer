946e956add8368264b1c82dcac3703e886661fa0
hoquangbao1@gmail.com <hoquangbao1@gmail.com>
2020-07-07 00:25:52 +0700

    Update job.controller.js

--- src/module/job/job.controller.js
+++ src/module/job/job.controller.js
@@ -20,7 +20,7 @@ export const getListJob = async (req, res) => {
 export const findJob = async (req, res) => {
   const offset = parseInt(req.query.offset, 10) || 0;
   const limit = parseInt(req.query.limit, 10) || 0;
-  const job = req.params.jobName;
+  const job = req.params;
   try {
     const listJob = await Job.find({ jobName: new RegExp(job, 'i'), isRemoved: false }
     ).skip(offset).limit(limit).populate('companyId', '_id companyName ' +
