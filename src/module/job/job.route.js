3dfb9fee566bdc846a423dcc392b6871a65806f1
hoquangbao1@gmail.com <hoquangbao1@gmail.com>
2020-07-07 00:27:47 +0700

    Update job.route.js

--- src/module/job/job.route.js
+++ src/module/job/job.route.js
@@ -8,8 +8,8 @@ import { authJwt } from '../../service/passport';
 const routes = new Router();
 
 routes.post('/create_job', authJwt, validate(jobValidate.createJob), jobController.createJob);
-routes.get('/', authJwt, jobController.getListJob);
-routes.get('/:jobName', authJwt, jobController.findJob);
+// routes.get('/', authJwt, jobController.getListJob);
+routes.get('/', authJwt, jobController.findJob);
 routes.get('/:id', authJwt, jobController.getJobDetail);
 routes.patch('/:id', authJwt, jobController.updateJob);
 routes.delete('/:id', authJwt, jobController.deleteJob);
